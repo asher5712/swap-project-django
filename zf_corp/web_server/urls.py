f2b18420b8f4def9b0bc096219795ed99b7ad9d1
Sher Ali <asher93905@gmail.com>
2022-05-28 12:40:39 +0500

    First Template (Home/Index) is introduced.

--- zf_corp/web_server/urls.py
+++ zf_corp/web_server/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from . import views
 
 # APP LEVEL URLS WILL BE WRITTEN HERE.
+urlpatterns = [
+    path('',views.index,name='serverIndex'),
+    path('home/',views.home,name='serverHome'),
+]
 
--- zf_corp/web_server/views.py
+++ zf_corp/web_server/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
+def index(request):
+    return redirect('/home')
+
+def home(request):
+    return render(request, 'web_server/index.html')
